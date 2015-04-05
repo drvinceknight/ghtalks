fc242e4b0860ce2e864f20192707710cbc813a2f
vince <vincent.knight@gmail.com>
2015-04-05 14:34:12 +0100

    Adding github repo url to setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='ghtalks',
     version='0.0.1',
     author='Vince Knight',
+    url='https://github.com/drvinceknight/ghtalks',
     author_email=('vincent.knight@gmail.com'),
     packages=['ghtalks', 'ghtalks.tests'],
     scripts=['bin/gh-talks'],
-    url='',
     license='The MIT License (MIT)',
     description='Generate a static side with talks',
     install_requires=[
