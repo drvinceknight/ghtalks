9f222e2d1d60edfaab3323db15182e25741ae432
vince <vincent.knight@gmail.com>
2015-04-05 16:19:23 +0100

    Re write tests to point at examples repository.

--- ghtalks/tests/test_write.py
+++ ghtalks/tests/test_write.py
@@ -8,28 +8,24 @@ class TestIndex(unittest.TestCase):
 
 
     expected_output="""
-<body>\n<div class="page-content">\n<div class="wrap">\n<div class="home">\n<ul class=\'posts\'>\n<li>\n<span class="post-date">2015-04-01 [.html]</span>\n<a class="post-link" href="./ghtalks/tests/test_directory/test_subdirectory/2015-04-01-test-directory/index.html">test directory</a>\n\n</li>\n<li>\n<span class="post-date">2015-03-31 [.html]</span>\n<a class="post-link" href="./ghtalks/tests/test_directory/2015-03-31-test-directory-2/index.html">test directory 2</a>\n\n</li>\n<li>\n<span class="post-date">2015-03-30 [.pdf]</span>\n<a class="post-link" href="./ghtalks/tests/test_directory/2015-03-30-test-directory-1/index.pdf">test directory 1</a>\n\n<p><a href=dummyurl.com>screencast</a></p>\n\n</li>\n</ul>\n</div>\n</div>\n</div>\n\n</body>\n"""
+<body>\n<div class="page-content">\n<div class="wrap">\n<div class="home">\n<ul class=\'posts\'>\n<li>\n<span class="post-date">2015-04-05 [.html]</span>\n<a class="post-link" href="./examples/2015-04-05-Using-ghtalks-to-organise-your-talks/index.html">Using ghtalks to organise your talks</a>\n\n</li>\n<li>\n<span class="post-date">2014-12-25 [.pdf]</span>\n<a class="post-link" href="./examples/archive/2014-12-25-Auraya/index.pdf">Auraya</a>\n\n</li>\n</ul>\n</div>\n</div>\n</div>\n\n</body>\n"""
 
     def test_init(self):
         root = os.getcwd()
         crwl = ghtalks.Crawl()
-        crwl.crawl_directories(os.path.join(root, 'ghtalks/tests'))
+        crwl.crawl_directories(os.path.join(root, 'examples'))
         index = ghtalks.Index(crwl.generate_talks())
         talk_titles = sorted([t.title for t in index.talks])
         talk_dates = sorted([t.date for t in index.talks])
         talk_paths = sorted([t.path for t in index.talks])
-        self.assertEqual(talk_titles, ['test directory', 'test directory 1',
-            'test directory 2'])
-        self.assertEqual(talk_dates, ['2015-03-30', '2015-03-31', '2015-04-01'])
-        self.assertEqual(talk_paths,
-                ['./ghtalks/tests/test_directory/2015-03-30-test-directory-1/index.pdf',
-                 './ghtalks/tests/test_directory/2015-03-31-test-directory-2/index.html',
-                 './ghtalks/tests/test_directory/test_subdirectory/2015-04-01-test-directory/index.html'])
+        self.assertEqual(talk_titles, ['Auraya', 'Using ghtalks to organise your talks'])
+        self.assertEqual(talk_dates, ['2014-12-25', '2015-04-05'])
+        self.assertEqual(talk_paths, ['./examples/2015-04-05-Using-ghtalks-to-organise-your-talks/index.html', './examples/archive/2014-12-25-Auraya/index.pdf'])
 
     def test_generate_output(self):
         root = os.getcwd()
         crwl = ghtalks.Crawl()
-        crwl.crawl_directories(os.path.join(root, 'ghtalks/tests'))
+        crwl.crawl_directories(os.path.join(root, 'examples'))
         index = ghtalks.Index(crwl.generate_talks())
         index.generate_output()
 
@@ -38,7 +34,7 @@ class TestIndex(unittest.TestCase):
     def test_write(self):
         root = os.getcwd()
         crwl = ghtalks.Crawl()
-        crwl.crawl_directories(os.path.join(root, 'ghtalks/tests'))
+        crwl.crawl_directories(os.path.join(root, 'examples'))
         index = ghtalks.Index(crwl.generate_talks())
         index.generate_output()
         index.write()
