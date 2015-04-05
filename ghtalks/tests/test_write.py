5e14661a7b8578a4b32bcaaa26df1889c1296e5d
vince <vincent.knight@gmail.com>
2015-04-05 10:21:04 +0100

    Adding the required methods to write the index.html file.

--- ghtalks/tests/test_write.py
+++ ghtalks/tests/test_write.py
@@ -18,6 +18,28 @@ class TestIndex(unittest.TestCase):
             'test directory 2'])
         self.assertEqual(talk_dates, ['2015-03-30', '2015-03-31', '2015-04-01'])
         self.assertEqual(talk_paths,
-                ['/ghtalks/tests/test_directory/2015-03-30-test-directory-1/index.pdf',
-                 '/ghtalks/tests/test_directory/2015-03-31-test-directory-2/index.html',
-                 '/ghtalks/tests/test_directory/test_subdirectory/2015-04-01-test-directory/index.html'])
+                ['./ghtalks/tests/test_directory/2015-03-30-test-directory-1/index.pdf',
+                 './ghtalks/tests/test_directory/2015-03-31-test-directory-2/index.html',
+                 './ghtalks/tests/test_directory/test_subdirectory/2015-04-01-test-directory/index.html'])
+
+    def test_generate_output(self):
+        root = os.getcwd()
+        crwl = ghtalks.Crawl()
+        crwl.crawl_directories(os.path.join(root, 'ghtalks/tests'))
+        index = ghtalks.Index(crwl.generate_talks())
+        index.generate_output()
+        expected_output="""
+<body>\n<div class="page-content">\n<div class="wrap">\n<div class="home">\n<ul class=\'posts\'>\n<li>\n<span class="post-date">2015-03-30 [.pdf]</span>\n<a class="post-link" href="./ghtalks/tests/test_directory/2015-03-30-test-directory-1/index.pdf">test directory 1</a>\n\n</li>\n<li>\n<span class="post-date">2015-03-31 [.html]</span>\n<a class="post-link" href="./ghtalks/tests/test_directory/2015-03-31-test-directory-2/index.html">test directory 2</a>\n\n</li>\n<li>\n<span class="post-date">2015-04-01 [.html]</span>\n<a class="post-link" href="./ghtalks/tests/test_directory/test_subdirectory/2015-04-01-test-directory/index.html">test directory</a>\n\n</li>\n</ul>\n</div>\n</div>\n</div>\n\n</body>\n"""
+        self.assertEqual(index.out, expected_output)
+
+    def test_write(self):
+        root = os.getcwd()
+        crwl = ghtalks.Crawl()
+        crwl.crawl_directories(os.path.join(root, 'ghtalks/tests'))
+        index = ghtalks.Index(crwl.generate_talks())
+        index.generate_output()
+        index.write()
+        expected_output="""
+<body>\n<div class="page-content">\n<div class="wrap">\n<div class="home">\n<ul class=\'posts\'>\n<li>\n<span class="post-date">2015-03-30 [.pdf]</span>\n<a class="post-link" href="./ghtalks/tests/test_directory/2015-03-30-test-directory-1/index.pdf">test directory 1</a>\n\n</li>\n<li>\n<span class="post-date">2015-03-31 [.html]</span>\n<a class="post-link" href="./ghtalks/tests/test_directory/2015-03-31-test-directory-2/index.html">test directory 2</a>\n\n</li>\n<li>\n<span class="post-date">2015-04-01 [.html]</span>\n<a class="post-link" href="./ghtalks/tests/test_directory/test_subdirectory/2015-04-01-test-directory/index.html">test directory</a>\n\n</li>\n</ul>\n</div>\n</div>\n</div>\n\n</body>\n"""
+        self.assertEqual(open('index.html', 'r').read(), expected_output)
+        os.remove('index.html')
--- ghtalks/write.py
+++ ghtalks/write.py
@@ -7,5 +7,43 @@ class Index:
     """
     Class that is passed a list of talks and writes the required html
     """
-    def __init__(self, talks, css_dir = False):
+    def __init__(self, talks, head=None, header=None, footer=None):
         self.talks = talks
+        self.head = head
+        self.header = header
+        self.footer = footer
+
+
+    def generate_output(self):
+        self.out = """"""
+        for f in [self.head, self.header]:
+            if f:
+                self.out += open(f, 'r').read()
+        self.out += """
+<body>
+<div class="page-content">
+<div class="wrap">
+<div class="home">
+<ul class='posts'>"""
+        for talk in self.talks:
+            self.out += """
+<li>"""
+            self.out += talk.html()
+            self.out += """
+</li>"""
+        self.out += """
+</ul>
+</div>
+</div>
+</div>
+"""
+        if self.footer:
+            self.out += open(self.footer, 'r').read()
+        self.out += """
+</body>
+"""
+
+    def write(self, output_file='index.html'):
+        f = open(output_file, 'w')
+        f.write(self.out)
+        f.close()
