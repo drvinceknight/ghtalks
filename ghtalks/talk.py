9b7c9fac91bad725d61cddd1c550a6bafc6a5049
vince <vincent.knight@gmail.com>
2015-04-05 10:20:36 +0100

    Adding file type and html method to talks.

--- ghtalks/talk.py
+++ ghtalks/talk.py
@@ -3,7 +3,15 @@ Data holder for a talk
 """
 
 class Talk:
-    def __init__(self, title, date, path):
+    def __init__(self, title, date, path, file_type):
         self.title = title
         self.date = date
         self.path = path
+        self.file_type = file_type
+
+    def html(self):
+        out = """
+<span class="post-date">%s [%s]</span>
+<a class="post-link" href="%s">%s</a>
+""" % (self.date, self.file_type, self.path, self.title)
+        return out
--- ghtalks/tests/test_talk.py
+++ ghtalks/tests/test_talk.py
@@ -7,7 +7,15 @@ import os
 class TestTalks(unittest.TestCase):
 
     def test_init(self):
-        talk = ghtalks.Talk('talk 1', '2015-04-01', './directory')
+        talk = ghtalks.Talk('talk 1', '2015-04-01', './directory', '.html')
         self.assertEquals(talk.title, 'talk 1')
         self.assertEquals(talk.date, '2015-04-01')
         self.assertEquals(talk.path, './directory')
+        self.assertEquals(talk.file_type, '.html')
+
+    def test_html(self):
+        talk = ghtalks.Talk('talk 1', '2015-04-01', './directory/index.html', '.html')
+        expected_out = """
+<span class="post-date">2015-04-01 [.html]</span>\n<a class="post-link" href="./directory/index.html">talk 1</a>
+"""
+        self.assertEquals(talk.html(), expected_out)
