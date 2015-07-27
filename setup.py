02b8c600ae95d9092638b6ff240d69490ac9d160
Vince Knight <vincent.knight@gmail.com>
2015-07-27 21:08:33 +0100

    New release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ghtalks',
-    version='0.0.2',
+    version='0.0.3',
     author='Vince Knight',
     url='https://github.com/drvinceknight/ghtalks',
     author_email=('vincent.knight@gmail.com'),
