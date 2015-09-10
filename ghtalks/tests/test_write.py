a070f09e22b6ccb4e38d779a1c88cb5f84b7e647
Vince Knight <vincent.knight@gmail.com>
2015-09-10 14:51:09 +0100

    Making bullet points for extra urls.

--- ghtalks/tests/test_write.py
+++ ghtalks/tests/test_write.py
@@ -8,7 +8,7 @@ class TestIndex(unittest.TestCase):
 
 
     expected_output="""
-<body>\n<div class="page-content">\n<div class="wrap">\n<div class="home">\n<ul class=\'posts\'>\n<li>\n<span class="post-date">2015-07-27 [.link]</span>\n<a class="post-link" href="http://vknight.org/Talks/\n">links</a>\n\n</li>\n<li>\n<span class="post-date">2015-04-05 [.html]</span>\n<a class="post-link" href="./examples/2015-04-05-Organise-and-share-talks/index.html">Organise and share talks</a>\n\n<p><a href=http://lab.hakim.se/reveal-js/#/>(slides written in reveal.js)</a></p>\n\n<p><a href=https://github.com/drvinceknight/ghtalks>github repository</a></p>\n\n</li>\n<li>\n<span class="post-date">2014-12-25 [.pdf]</span>\n<a class="post-link" href="./examples/archive/2014-12-25-Auraya/index.pdf">Auraya</a>\n\n</li>\n</ul>\n</div>\n</div>\n</div>\n\n</body>\n"""
+<body>\n<div class="page-content">\n<div class="wrap">\n<div class="home">\n<ul class=\'posts\'>\n<li>\n<span class="post-date">2015-07-27 [.link]</span>\n<a class="post-link" href="http://vknight.org/Talks/\n">links</a>\n\n</li>\n<li>\n<span class="post-date">2015-04-05 [.html]</span>\n<a class="post-link" href="./examples/2015-04-05-Organise-and-share-talks/index.html">Organise and share talks</a>\n<ul>\n<li><a href=http://lab.hakim.se/reveal-js/#/>(slides written in reveal.js)</a></li>\n\n<li><a href=https://github.com/drvinceknight/ghtalks>github repository</a></li>\n</ul>\n</li>\n<li>\n<span class="post-date">2014-12-25 [.pdf]</span>\n<a class="post-link" href="./examples/archive/2014-12-25-Auraya/index.pdf">Auraya</a>\n\n</li>\n</ul>\n</div>\n</div>\n</div>\n\n</body>\n"""
 
     def test_init(self):
         root = os.getcwd()
